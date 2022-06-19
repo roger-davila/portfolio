e70314c17fa83b9b3fd8d1faa1f4e521354c73c6
Roger Davila <rogerddavila@gmail.com>
2022-06-19 16:47:11 -0700

    add routes and remove logout function

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,24 @@
 import { Link } from 'react-router-dom';
+import { MenuIcon, LightIcon } from '../PortfolioIcons/PortfolioIcons';
 import * as userService from '../../utilities/users-service';
+import './NavBar.css';
 
-export default function NavBar({ user, setUser }) {
+export default function NavBar() {
 
-  function handleLogOut() {
-    // Remove token using the user service
-    userService.logOut();
-    // Update user state in App
-    setUser(null);
-  }
+  // function handleLogOut() {
+  //   // Remove token using the user service
+  //   userService.logOut();
+  //   // Update user state in App
+  //   setUser(null);
+  // }
 
   return (
-    <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp; | &nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp; | &nbsp;
-      <Link to="" onClick={handleLogOut}>Log Out</Link>
+    <nav className='NavBar'>
+      <MenuIcon />
+      <Link className='NavBar-item' to="/">Home</Link>
+      <Link className='NavBar-item' to="/work">Work</Link>
+      <Link className='NavBar-item' to="/resume">Resume</Link>
+      <LightIcon />
     </nav>
   );
 }
