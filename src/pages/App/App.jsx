be3fc37bcae211fc3ea404386abe7c8da5052f21
Roger Davila <rogerddavila@gmail.com>
2022-06-19 16:49:18 -0700

    remove unused styling and add update routes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,24 +2,22 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
+import WorkPage from '../WorkPage/WorkPage';
 import NavBar from '../../components/NavBar/NavBar';
+import HeroName from '../../components/HeroName/HeroName';
 import './App.css';
 
 function App() {
   const [user, setUser] = useState(getUser());
 
   return (
-    <main className="App">
-      { user ?
-        <>
-          <NavBar user={user} setUser={setUser} />
-          <Routes>
-            {/* Route components in here */}
-          </Routes>
-        </>
-        :
-        <AuthPage setUser={setUser} />
-      }
+    <main className='App'>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<HeroName />}/>
+        <Route path='/work' element={<WorkPage />}/>
+        {/* Route components in here */}
+      </Routes>
     </main>
   );
 }
