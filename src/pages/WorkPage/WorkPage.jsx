72a3f2622e3352541c503698610d1d1ec04d0d1f
Roger Davila <rogerddavila@gmail.com>
2022-06-19 21:10:11 -0700

    import project data and create project components

--- src/pages/WorkPage/WorkPage.jsx
+++ src/pages/WorkPage/WorkPage.jsx
@@ -1,7 +1,14 @@
+import ProjectCard from '../../components/ProjectCard/ProjectCard';
+import projectsData from '../../projects.json';
+
 export default function WorkPage() {
+  let projects = projectsData.map((project, idx) => <ProjectCard key={idx} project={project} />);
   return (
     <main className='WorkPage main-container'>
       <h1>Work</h1>
+      <section>
+        {projects}
+      </section>
     </main>
   );
 }
