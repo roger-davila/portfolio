fdc82f2db921491b59f1a1141c2b1781e4fe15e2
Roger Davila <rogerddavila@gmail.com>
2022-06-20 20:32:02 -0700

    add containers to project links

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -7,14 +7,18 @@ export default function ProjectCard({ project }) {
       <h2 className='project-name'>{project.name}</h2>
       <p className='project-description'>{project.description}</p>
       <section className='project-links'>
-        <a href={project.githubLink}>
-          <GitHubIcon />
-          <p>GitHub</p>
-        </a>
-        <a href={project.deployedLink}>
-          <ExternalLinkIcon />
-          <p>Deployed</p>
-        </a>
+        <div>
+          <a href={project.githubLink}>
+            <GitHubIcon classString='link-icon' />
+            <p>GitHub</p>
+          </a>
+        </div>
+        <div>
+          <a href={project.deployedLink}>
+            <ExternalLinkIcon classString='link-icon' />
+            <p>Deployed</p>
+          </a>
+        </div>
       </section>
     </article>
   );
