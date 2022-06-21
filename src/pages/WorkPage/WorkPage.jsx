56ebd92771eea6e7120c98167194c3ee2aafb5df
Roger Davila <rogerddavila@gmail.com>
2022-06-20 20:33:57 -0700

    add styling and classname for main page header

--- src/pages/WorkPage/WorkPage.jsx
+++ src/pages/WorkPage/WorkPage.jsx
@@ -1,11 +1,12 @@
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import projectsData from '../../projects.json';
+import './WorkPage.css';
 
 export default function WorkPage() {
   let projects = projectsData.map((project, idx) => <ProjectCard key={idx} project={project} />);
   return (
     <main className='WorkPage main-container'>
-      <h1>Work</h1>
+      <h1 className='main-header'>My Projects</h1>
       <section>
         {projects}
       </section>
