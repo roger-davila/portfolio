e48b00908c822718e15cacd58462ae119bf25145
Roger Davila <rogerddavila@gmail.com>
2022-06-20 20:30:59 -0700

    add styling and onlclick events for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { MenuIcon, LightIcon } from '../PortfolioIcons/PortfolioIcons';
+import { MenuIcon, LightIcon, XIcon } from '../PortfolioIcons/PortfolioIcons';
 import * as userService from '../../utilities/users-service';
 import './NavBar.css';
 
 export default function NavBar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
+  function handleClick() {
+    setIsNavOpen(!isNavOpen);
+  }
   // function handleLogOut() {
   //   // Remove token using the user service
   //   userService.logOut();
@@ -13,12 +18,15 @@ export default function NavBar() {
   // }
 
   return (
-    <nav className='NavBar'>
-      <MenuIcon />
-      <Link className='NavBar-item' to="/">Home</Link>
-      <Link className='NavBar-item' to="/work">Work</Link>
-      <Link className='NavBar-item' to="/resume">Resume</Link>
-      <LightIcon />
-    </nav>
+    <div className='NavBar-wrapper'>
+      <MenuIcon classString='open button nav-icon' handleClick={handleClick} />
+      <LightIcon classString='button nav-icon' />
+      <nav className={`NavBar ${isNavOpen ? 'active' : ''}`}>
+        <XIcon classString='close button nav-icon' handleClick={handleClick} />
+        <Link className='NavBar-item' onClick={handleClick} to="/">Home</Link>
+        <Link className='NavBar-item' onClick={handleClick} to="/work">Work</Link>
+        <Link className='NavBar-item' onClick={handleClick} to="/resume">Resume</Link>
+      </nav>
+    </div>
   );
 }
